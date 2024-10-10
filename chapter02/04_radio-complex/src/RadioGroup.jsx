896ea41fb2b56c6325831b057dc964d61a17ce47
StevenLiu <stevenliu986@gmail.com>
2024-10-10 13:10:50 +1100

    add more options

---
+++ chapter02/04_radio-complex/src/RadioGroup.jsx
@@ -0,0 +1,26 @@
+import {useState} from "react";
+
+export default function RadioGroup({name, options, onChange}) {
+    const [selectedValue, setSelectedValue] = useState("");
+
+    const handleChange = (e) => {
+        setSelectedValue(e.target.value);
+        if (onChange) {
+            onChange(e.target.value);
+        }
+    }
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+        }}>
+            {options.map(option => (
+                <label key={option}>
+                    <input type="radio" name={name} value={option} checked={selectedValue === option}
+                           onChange={handleChange}/>{option}
+                </label>
+            ))}
+        </div>
+    )
+}
