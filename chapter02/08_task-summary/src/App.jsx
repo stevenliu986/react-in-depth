55264dd740b9b20ec15055822c3abc73edb9d9b1
StevenLiu <stevenliu986@gmail.com>
2024-10-11 22:39:46 +1100

    task manager init

---
+++ chapter02/08_task-summary/src/App.jsx
@@ -0,0 +1,11 @@
+import { LongTaskManager } from "./LongTaskManager.jsx"; // import {CompactTaskManager} from "./CompactTaskManager.jsx";
+// import {CompactTaskManager} from "./CompactTaskManager.jsx";
+
+export default function App() {
+  return (
+    <main>
+      <LongTaskManager />
+      {/* <CompactTaskManager /> */}
+    </main>
+  );
+}
