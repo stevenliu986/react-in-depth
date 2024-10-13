f315619d9c8b6388c6281ae2065ed0eb907bec02
StevenLiu <stevenliu986@gmail.com>
2024-10-13 19:56:30 +1100

    update codes

--- chapter03/02_strict-mode/src/App.jsx
+++ chapter03/02_strict-mode/src/App.jsx
@@ -1,12 +1,31 @@
-import { useState, useRef, useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
+
+let outsideCount = 0;
 
 function App() {
-  return (
-    <main>
-      <h4>Re-renders without changing properties</h4>
-      <h4>No re-renders with changing properties</h4>
-    </main>
-  );
+  const ref = useRef(true);
+  const [count, setCount] = useState(() => {
+    console.log("initialising count to", outsideCount);
+    return outsideCount++;
+  });
+
+  console.log("rendering with ", count);
+  useEffect(() => {
+    console.log("effect first time?", ref.current);
+    ref.current = false;
+    setCount((c) => {
+      console.log("setting count from ", c, " to ", c + 1);
+      return c + 1;
+    });
+    return () => console.log("cleaning up");
+  }, []);
+
+  useEffect(() => {
+    console.log("effect every time?", count);
+    return () => console.log("cleaning up every time");
+  }, [count]);
+
+  return <h1>What is the count? {count}</h1>;
 }
 
 export default App;
