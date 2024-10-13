163a1c467ed6082d07f63806b7f82eb32e74784b
StevenLiu <stevenliu986@gmail.com>
2024-10-13 13:02:37 +1100

    add lodash

--- chapter02/task-summary/src/App.jsx
+++ chapter02/task-summary/src/App.jsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return <main>form</main>;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleMode = () => setIsLoading((v) => !v);
+
+  const style = {
+    isLoading: {
+      color: "red",
+    },
+    loaded: {
+      color: "green",
+    },
+  };
+
+  useEffect(() => {
+    console.log("useEffect called.");
+  }, []);
+  return (
+    <main>
+      {isLoading ? (
+        <h1 style={style.isLoading}>The page is loading, please wait.</h1>
+      ) : (
+        <h1 style={style.loaded}>Page is loaded.</h1>
+      )}
+      <button onClick={toggleMode}>模式切换</button>
+    </main>
+  );
 }
