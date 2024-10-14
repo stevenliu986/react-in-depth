7b4799f78bc728123469fbd652d1c6abc2b89e6a
StevenLiu <stevenliu986@gmail.com>
2024-10-14 18:05:32 +1100

    update code

--- chapter03/03_todo-simple/src/App.jsx
+++ chapter03/03_todo-simple/src/App.jsx
@@ -1,25 +1,23 @@
 import { useState } from "react";
 
-// item组件
 function Items({ items }) {
   return (
-    <ul>
-      {items.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
-    </ul>
+    <>
+      <h2>Todo items</h2>
+      <ul>
+        {items.map((item) => {
+          <li key={item}>{item}</li>;
+        })}
+      </ul>
+    </>
   );
 }
 
-// Todo组件
 function Todo() {
+  const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState("");
-  const [items, setItems] = useState(["Clean gutter", "Wash dishes"]);
 
   const onSubmit = (evt) => {
-    if (newItem.trim() === "") {
-      return;
-    }
     setItems((items) => items.concat([newItem]));
     setNewItem("");
     evt.preventDefault();
@@ -27,13 +25,13 @@ function Todo() {
 
   const onChange = (evt) => setNewItem(evt.target.value);
   return (
-    <main>
+    <>
       <form onSubmit={onSubmit}>
         <input type="text" value={newItem} onChange={onChange} />
-        <button>Add task</button>
+        <button>Add todo item</button>
       </form>
       <Items items={items} />
-    </main>
+    </>
   );
 }
 
