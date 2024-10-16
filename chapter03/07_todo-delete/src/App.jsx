74616507592e06fbced97111380a099157fc8a0e
StevenLiu <stevenliu986@gmail.com>
2024-10-16 20:55:35 +1100

    update App

--- chapter03/07_todo-delete/src/App.jsx
+++ chapter03/07_todo-delete/src/App.jsx
@@ -1,5 +1,39 @@
+import { memo, useState } from "react";
+
+const Items = memo(function Items({ items }) {
+  return (
+    <>
+      <h1>Todo Items</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+});
+
 function Todo() {
-  return <h1>todo</h1>;
+  const [items, setItems] = useState(["Wash dishes", "Clean table"]);
+  const [newItem, setNewItem] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setItems([...items, newItem]);
+    setNewItem("");
+  };
+
+  const handleChange = (evt) => setNewItem(evt.target.value);
+
+  return (
+    <>
+      <Items items={items} />
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={newItem} onChange={(e) => handleChange(e)} />
+        <button>Add</button>
+      </form>
+    </>
+  );
 }
 
 function App() {
