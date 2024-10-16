aeede120c46e0ce1b9e9a5e45a40944fe682a89e
StevenLiu <stevenliu986@gmail.com>
2024-10-16 12:29:11 +1100

    todo usememo init

---
+++ chapter03/05_todo-usememo/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState, memo } from "react";
+
+const Items = memo(function Items({ items }) {
+  return (
+    <>
+      <h2>Todo items</h2>
+      <ul>
+        {items.map((item) => {
+          <li key={item}>{item}</li>;
+        })}
+      </ul>
+    </>
+  );
+});
+
+function Todo() {
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState("");
+
+  const onSubmit = (evt) => {
+    setItems((items) => items.concat([newItem]));
+    setNewItem("");
+    evt.preventDefault();
+  };
+
+  const onChange = (evt) => setNewItem(evt.target.value);
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={newItem} onChange={onChange} />
+        <button>Add todo item</button>
+      </form>
+      <Items items={items} />
+    </>
+  );
+}
+
+function App() {
+  return <Todo />;
+}
+
+export default App;
