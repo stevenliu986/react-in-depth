a56ae984d004d4cf031003d5a44d06c6aaa45281
StevenLiu <stevenliu986@gmail.com>
2024-10-16 17:39:45 +1100

    todo delete init

--- chapter03/07_todo-delete/src/App.jsx
+++ chapter03/07_todo-delete/src/App.jsx
@@ -1,39 +1,5 @@
-import { useState, memo, useMemo } from "react";
-
-const Items = memo(function Items({ items }) {
-  return (
-    <>
-      <h2>Todo items</h2>
-      <ul>
-        {items.map((item) => {
-          <li key={item}>{item}</li>;
-        })}
-      </ul>
-    </>
-  );
-});
-
 function Todo() {
-  const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState("");
-
-  const onSubmit = (evt) => {
-    setItems((items) => items.concat([newItem]));
-    setNewItem("");
-    evt.preventDefault();
-  };
-
-  const onChange = (evt) => setNewItem(evt.target.value);
-  const allItems = useMemo(() => ["Complete todo list", ...items], [items]);
-  return (
-    <>
-      <form onSubmit={onSubmit}>
-        <input type="text" value={newItem} onChange={onChange} />
-        <button>Add todo item</button>
-      </form>
-      <Items items={allItems} />
-    </>
-  );
+  return <h1>todo</h1>;
 }
 
 function App() {
