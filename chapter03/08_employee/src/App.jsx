da6154add8823c857be279ca4cd7da2cdc8f8050
StevenLiu <stevenliu986@gmail.com>
2024-10-17 11:22:58 +1100

    update App

--- chapter03/08_employee/src/App.jsx
+++ chapter03/08_employee/src/App.jsx
@@ -1,9 +1,52 @@
-function Employee() {
-  return <h1>employee</h1>;
+import { useState, useMemo } from "react";
+
+function Employee({ employeeList }) {
+  const [showTemp, setShowTemp] = useState(false);
+
+  const filteredEmployeeList = useMemo(
+    () =>
+      employeeList.filter((employee) =>
+        showTemp ? employee.isTemporary : true,
+      ),
+    [showTemp, employeeList],
+  );
+  return (
+    <>
+      <h1>Employee List</h1>
+      <br />
+      <label>
+        <input type="checkbox" onChange={() => setShowTemp((v) => !v)} />
+        Show temporary employees only?
+      </label>
+
+      <ul>
+        {filteredEmployeeList.map((employee) => (
+          <li key={employee.name}>
+            {employee.name}: {employee.salary}
+            {employee.isTemporary ? "(temp)" : null}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 function App() {
-  return <Employee />;
+  const employeeList = [
+    { name: "Bugs Bunny", salary: "$20,000", isTemporary: false },
+    { name: "Daffy Duck", salary: "$15,000", isTemporary: false },
+    { name: "Porky Pig", salary: "$17,000", isTemporary: true },
+    { name: "Elmer Fudd", salary: "$18,500", isTemporary: false },
+    { name: "Wile E. Coyote", salary: "$21,000", isTemporary: false },
+    { name: "Road Runner", salary: "$22,000", isTemporary: false },
+    { name: "Tweety", salary: "$7,000", isTemporary: false },
+    { name: "Sylvester", salary: "$59,000", isTemporary: false },
+    { name: "Granny", salary: "$6,900", isTemporary: true },
+    { name: "Yosemite Sam", salary: "$11,000", isTemporary: true },
+    { name: "Tasmanian Devil", salary: "$12,000", isTemporary: true },
+  ];
+
+  return <Employee employeeList={employeeList} />;
 }
 
 export default App;
