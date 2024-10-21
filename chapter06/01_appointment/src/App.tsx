1bf495318bf9cc9ddc80e44c8ced68ca4638b5df
StevenLiu <stevenliu986@gmail.com>
2024-10-21 17:15:07 +1100

    update code

--- chapter06/01_appointment/src/App.tsx
+++ chapter06/01_appointment/src/App.tsx
@@ -1,9 +1,29 @@
 import "./app.css";
 
 function App() {
+  const user = {
+    name: "John Doe",
+    date: "2021-09-01",
+  };
   return (
     <>
-      <h1> Appointment</h1>
+      <h1>Incoming Appointment</h1>
+
+      <p>From: {user.name}</p>
+      <p>Date: {user.date}</p>
+      <div>
+        <label>
+          <input type="radio" /> Accepted
+        </label>
+        <label>
+          <input type="radio" /> Declined
+        </label>
+      </div>
+
+      <label>
+        Optional Messages:
+        <input type="text" />
+      </label>
     </>
   );
 }
