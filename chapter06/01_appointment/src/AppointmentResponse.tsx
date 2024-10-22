3618a4020f14d6ffd6a389d81d354d75d72f59f0
StevenLiu <stevenliu986@gmail.com>
2024-10-22 19:18:55 +1100

    update code

--- chapter06/01_appointment/src/AppointmentResponse.tsx
+++ chapter06/01_appointment/src/AppointmentResponse.tsx
@@ -11,51 +11,49 @@ export default function AppointmentResponse({
   const [message, setMessage] = useState<string>("");
   const canSubmit = typeof isAccepted === "boolean";
   return (
-    <>
-      <fieldset
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "1em",
-          width: "300px",
-        }}
-      >
-        <legend>Appointment Response</legend>
-        <div>
-          <label>
-            <input
-              type="radio"
-              checked={isAccepted === true}
-              onChange={() => setIsAccepted(true)}
-            />{" "}
-            Accepted
-          </label>
-          <label>
-            <input
-              type="radio"
-              checked={isAccepted === false}
-              onChange={() => setIsAccepted(false)}
-            />{" "}
-            Declined
-          </label>
-        </div>
-
+    <fieldset
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "1em",
+        width: "300px",
+      }}
+    >
+      <legend>Appointment Response</legend>
+      <div>
         <label>
-          Optional Messages:{" "}
           <input
-            type="text"
-            value={message}
-            placeholder="Message"
-            onChange={(event) => setMessage(event.target.value)}
-          />
+            type="radio"
+            checked={isAccepted === true}
+            onChange={() => setIsAccepted(true)}
+          />{" "}
+          Accepted
         </label>
-        <button
-          disabled={!canSubmit}
-          onClick={() => canSubmit && onSubmit({ isAccepted, message })}
-        >
-          Submit
-        </button>
-      </fieldset>
-    </>
+        <label>
+          <input
+            type="radio"
+            checked={isAccepted === false}
+            onChange={() => setIsAccepted(false)}
+          />{" "}
+          Declined
+        </label>
+      </div>
+
+      <label>
+        Optional Messages:{" "}
+        <input
+          type="text"
+          value={message}
+          placeholder="Message"
+          onChange={(event) => setMessage(event.target.value)}
+        />
+      </label>
+      <button
+        disabled={!canSubmit}
+        onClick={() => canSubmit && onSubmit({ isAccepted, message })}
+      >
+        Submit
+      </button>
+    </fieldset>
   );
 }
