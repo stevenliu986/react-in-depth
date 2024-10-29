40046074ed121b3b186c6781fbd75f5494eb0e88
StevenLiu <stevenliu986@gmail.com>
2024-10-29 22:32:18 +1100

    update code

--- chapter07/04_styled-components/src/Button.jsx
+++ chapter07/04_styled-components/src/Button.jsx
@@ -1,52 +1,64 @@
-const BUTTON_STYLES = {
-  display: "flex",
-  background: "hotpink",
-  color: "white",
-  border: "2px solid hotpink",
-  margin: "1em 2em",
-  padding: "1em 2em",
-  fontSize: "120%",
-  width: "250px",
-  cursor: "pointer",
-  gap: "10px",
-  justifyContent: "center",
-  alignItems: "center",
-};
-const OUTLINE_STYLES = {
-  borderColor: "hotpink",
-  backgroundColor: "transparent",
-  color: "hotpink",
-};
-const DISABLED_STYLES = {
-  opacity: 0.5,
-  pointerEvents: "none",
-};
-const ICON_STYLES = {
-  width: "16px",
-  height: "16px",
-};
+import styled, { keyframes } from "styled-components";
+
+const Icon = styled.svg`
+  width: 16px;
+  height: 16px;
+  position: relative;
+`;
+
+const bounce = keyframes`
+    from {
+        left: 0;
+    }
+    to {
+        left: 10px;
+    }
+`;
+
+const Normal = styled.button`
+  display: flex;
+  background: hotpink;
+  color: white;
+  border: 2px solid hotpink;
+  margin: 1em 2em;
+  padding: 1em 2em;
+  font-size: 120%;
+  width: 250px;
+  cursor: pointer;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
+
+  ${({ $width }) => $width && `width: ${$width}px`};
+
+  &:hover ${Icon} {
+    animation: ${bounce} 0.2s ease-in-out alternate infinite;
+  }
+
+  &[disabled] {
+    opacity: 0.5;
+    pointer-events: none;
+  }
+`;
+
+const Outline = styled(Normal)`
+  border-color: hotpink;
+  background-color: transparent;
+  color: hotpink;
+`;
 
 function Button({
   children,
   outline = false,
   disabled = false,
-  style = {},
+  className = null,
   width = null,
 }) {
-  const elementStyle = {
-    ...BUTTON_STYLES,
-    ...(outline && OUTLINE_STYLES),
-    ...(disabled && DISABLED_STYLES),
-    ...(width && {
-      width: `${width}px`,
-    }),
-    ...style,
-  };
+  const Element = outline ? Outline : Normal;
   return (
-    <button disabled={disabled} style={elementStyle}>
+    <Element disabled={disabled} $width={width} className={className}>
       {children}
-      <svg
-        style={ICON_STYLES}
+      <Icon
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
@@ -56,8 +68,8 @@ function Button({
           points="240.112,0 481.861,245.004 240.112,490 8.139,490 250.29,245.004 8.139,0"
           fill="currentColor"
         />
-      </svg>
-    </button>
+      </Icon>
+    </Element>
   );
 }
 
