ec095fed01f2653fed375839fcfd0b31b97cb92a
StevenLiu <stevenliu986@gmail.com>
2024-10-30 22:27:22 +1100

    insert provider

--- chapter08/01_context/src/App.jsx
+++ chapter08/01_context/src/App.jsx
@@ -1,11 +1,12 @@
 import { Things } from "./things";
+import { DataProvider } from "./data/index.js";
 
 function App() {
   return (
-    <>
+    <DataProvider>
       <h1>All my things</h1>
       <Things />
-    </>
+    </DataProvider>
   );
 }
 
