299ad9d1a874ece74d06472172f880596039ec1c
StevenLiu <stevenliu986@gmail.com>
2024-11-04 12:07:01 +1100

    update comment

--- chapter08/02_redux/src/App.jsx
+++ chapter08/02_redux/src/App.jsx
@@ -2,8 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { addThing } from "./dataReducer.js";
 
 function App() {
-  // state.thingReducer.data：thingReducer是指创建store时，指定的reducer的名称。而这里的data是指initialSate中的key(data)
-  // 也就是说，state是全局对象，保存了thingReducer这个对象，而这个对象是dataReducer生成的
+  // state.thingReducer.data：thingReducer是指创建store时，指定的reducer的名称。而这里的data是指initialSate中的key(data)。也就是说，state是全局对象，保存了thingReducer这个对象，而这个对象是dataReducer生成的。
   const result = useSelector((state) => state.thingReducer.data);
   const dispatch = useDispatch();
   console.log(result);
