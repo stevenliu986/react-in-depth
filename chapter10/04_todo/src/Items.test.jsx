8b529d59c8b20e727fef683afa81a837094b11f4
StevenLiu <stevenliu986@gmail.com>
2024-11-06 16:11:39 +1100

    add new test case

--- chapter10/04_todo/src/Items.test.jsx
+++ chapter10/04_todo/src/Items.test.jsx
@@ -1,31 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
-import { expect, test } from "vitest";
+import { expect, test, vi } from "vitest";
 import { Items } from "./Items.jsx";
 
-function setup(start) {
-  render(<Items start={start} />);
-  const heading = screen.getByRole("heading", { name: `Counter: ${start}` });
+test("Items should call onDelete callback when item is deleted", async () => {
+  const items = ["Item A", "Item B", "Item C"];
+  const mockDelete = vi.fn();
+  render(<Items items={items} onDelete={mockDelete} />);
   const user = userEvent.setup();
-
-  return { heading, user };
-}
-
-test("Items should start at the given value", () => {
-  const { heading } = setup(10);
-  expect(heading).toBeInTheDocument();
-});
-
-test("Items should start at the default value", () => {
-  const { heading } = setup(0);
-  expect(heading).toBeInTheDocument();
-});
-
-test("Items should increment when clicking the button", async () => {
-  const { user } = setup(0);
-  const incrementButton = screen.getByRole("button", { name: "Increment" });
-  await user.click(incrementButton);
-  const heading = screen.getByRole("heading", { name: "Counter: 1" });
-  expect(heading).toBeInTheDocument();
+  const secondItem = screen.getByRole("button", { name: `Delete 'Item B'` });
+  await user.click(secondItem);
+  expect(mockDelete).toHaveBeenCalledWith("Item B");
 });
