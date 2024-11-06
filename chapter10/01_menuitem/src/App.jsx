4d30cd15e7d27259272768239580d38ed59ada99
StevenLiu <stevenliu986@gmail.com>
2024-11-06 11:24:16 +1100

    insert new component

--- chapter10/01_menuitem/src/App.jsx
+++ chapter10/01_menuitem/src/App.jsx
@@ -1,32 +1,10 @@
-import { useState } from "react";
+import { MenuItem } from "./MenuItem";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ul>
+      <MenuItem href="/blog" label="Blog" />
+      <MenuItem href="/about" label="About" />
+    </ul>
   );
 }
-
-export default App;
