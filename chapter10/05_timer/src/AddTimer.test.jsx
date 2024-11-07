3f906a83d45266208e52439df5539442c47f4ce8
StevenLiu <stevenliu986@gmail.com>
2024-11-07 16:21:08 +1100

    add test case

--- chapter10/05_timer/src/AddTimer.test.jsx
+++ chapter10/05_timer/src/AddTimer.test.jsx
@@ -18,4 +18,15 @@ test("Counter should start at the given value", async () => {
   const { onAdd, minutes, seconds, start, user } = setup();
   expect(minutes).toHaveValue(0);
   expect(seconds).toHaveValue(0);
+
+  await user.type(minutes, "5");
+  await user.type(seconds, "30");
+  expect(minutes).toHaveValue(5);
+  expect(seconds).toHaveValue(30);
+
+  await user.click(start);
+  const expectValue = 5 * 60 + 30;
+  expect(onAdd).toHaveBeenCalledWith(expectValue);
+  expect(minutes).toHaveValue(0);
+  expect(seconds).toHaveValue(0);
 });
